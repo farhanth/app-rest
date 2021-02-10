93fee40bd0d4311f05dbc54cfd8282a9f0d2aeb2
farhanth <farhantaufik98@gmail.com>
2021-02-10 23:41:51 +0700

    Create getMahasiswaByID

--- controller.js
+++ controller.js
@@ -7,6 +7,7 @@ exports.index = function (req, res) {
     response.ok("Rest running successfully", res)
 };
 
+//get all mahasiswa
 exports.getMahasiswa = function (req, res) {
     connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
         if (error) {
@@ -16,3 +17,16 @@ exports.getMahasiswa = function (req, res) {
         }
     });
 };
+
+//get mahasiswa by ID
+exports.getMahasiswaByID = function (req, res) {
+    let id = req.params.id;
+
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id], function (error, rows, fields) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+}
