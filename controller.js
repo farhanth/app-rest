380738868f26e755e29dfb2975132e02862f6cc9
farhanth <farhantaufik98@gmail.com>
2021-02-10 22:46:23 +0700

    Add controller file

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./connection');
 
 exports.index = function (req, res) {
-    response.ok("Rest running successfully")
+    response.ok("Rest running successfully", res)
 };
