dcbc869a476a48cafbec6a8f01b575c5bb6b0ad7
farhanth <farhantaufik98@gmail.com>
2021-02-10 23:26:55 +0700

    Create getMahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,13 @@ var connection = require('./connection');
 exports.index = function (req, res) {
     response.ok("Rest running successfully", res)
 };
+
+exports.getMahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+};
