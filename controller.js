3bb0c8924224e5e49303d28c8054b50e01ccfc16
farhanth <farhantaufik98@gmail.com>
2021-02-11 00:01:35 +0700

    Create addMahasiswa

--- controller.js
+++ controller.js
@@ -22,11 +22,31 @@ exports.getMahasiswa = function (req, res) {
 exports.getMahasiswaByID = function (req, res) {
     let id = req.params.id;
 
-    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id], function (error, rows, fields) {
-        if (error) {
-            console.log(error);
-        } else {
-            response.ok(rows, res);
-        }
-    });
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?',
+        [id],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        });
 }
+
+//add mahasiswa
+
+exports.addMahasiswa = function (req, res) {
+    var npm = req.body.npm;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (npm, nama, jurusan) VALUES (?, ?, ?)',
+        [npm, nama, jurusan],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Data mahasiswa successfully added", res);
+            }
+        });
+};
